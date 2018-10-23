f4e4ca6cb60d53093612837fcc481ca1954aae6d
Jacob Egner <jmegner@gmail.com>
2018-10-23 18:52:41 -0500

    v0.1, add some notes and update metadata block

--- google_flights_get_unavailable_prices.user.js
+++ google_flights_get_unavailable_prices.user.js
@@ -1,19 +1,60 @@
 // ==UserScript==
-// @name        google_flights_fiddle
-// @namespace   JacobEgner
-// @version     0.0
-// @description for fiddling with Project01_GoogleFlights
-// @license     CC0-1.0
+// @name        google_flights_get_unavailable_prices
+// @namespace   https://github.com/jmegner
+// @version     0.1
+// @description for getting unavailable prices for google flights results
+// @license     Unlicense
+// @homepageURL https://github.com/jmegner/greasemonkey_google_flights_get_unavailable_prices
+// @supportURL  https://github.com/jmegner/greasemonkey_google_flights_get_unavailable_prices/issues
 // @match       https://www.google.com/flights*
-// @run-at document-idle
+// @grant       GM.xmlHttpRequest
+// @run-at      document-end
 // ==/UserScript==
 
-console.log("jme begin");
+console.debug("jme begin");
 
+(new MutationObserver(checkForResults)).observe(
+  document,
+  {childList: true, subtree: true});
+
+/* // example page request for later reference
+GM.xmlHttpRequest({
+  method: "GET",
+  url: "http://www.google.com/",
+  onload: function(response) {
+    console.debug("jme response...");
+    console.debug(response);
+    var responseDom = new DOMParser().parseFromString(response.responseText, "text/html");
+    console.debug("jme parsed response...");
+    console.debug(responseDom);
+  }
+});
+*/
+
+/* // southwest URL for later reference
+var southwestUrl = "https://www.southwest.com/air/booking/select.html"
+  + "?originationAirportCode=HOU"
+  + "&destinationAirportCode=FLL"
+  + "&returnAirportCode="
+  + "&departureDate=2018-11-07"
+  + "&departureTimeOfDay=ALL_DAY"
+  + "&returnDate=2018-11-11"
+  + "&returnTimeOfDay=ALL_DAY"
+  + "&adultPassengersCount=1"
+  + "&seniorPassengersCount=0"
+  + "&fareType=USD"
+  + "&passengerType=ADULT"
+  + "&tripType=roundtrip"
+  + "&promoCode="
+  + "&reset=true"
+  + "&redirectToVision=true"
+  + "&int=HOMEQBOMAIR"
+  + "&leapfrogRequest=true";
+*/
 
 function checkForResults(changes, observer)
 {
-  //console.log("jme checkForResults begin");
+  //console.debug("jme checkForResults begin");
   var dominatedList = document.querySelector('ol.gws-flights-results__has-dominated');
 
   if(dominatedList)
@@ -22,19 +63,19 @@ function checkForResults(changes, observer)
     modifySouthwestEntries(dominatedList);
   }
 
-  //console.log("jme checkForResults end")
+  //console.debug("jme checkForResults end")
 }
 
 
 function modifySouthwestEntries(dominatedList)
 {
-  console.log("jme modifySouthwestEntries begin");
+  console.debug("jme modifySouthwestEntries begin");
 
   var southwestFlightXPathResult = document.evaluate(
     ".//li[contains(@class, 'gws-flights-results__result-item') and contains(.//span, 'Southwest')]",
     dominatedList, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
 
-  console.log("jme found " + southwestFlightXPathResult.snapshotLength + " flights");
+  console.debug("jme found " + southwestFlightXPathResult.snapshotLength + " flights");
 
   for(var snapshotIdx = 0; snapshotIdx < southwestFlightXPathResult.snapshotLength; snapshotIdx++)
   {
@@ -42,14 +83,14 @@ function modifySouthwestEntries(dominatedList)
     modifySouthwestEntry(southwestFlightListItem);
   }
 
-  console.log("jme modifySouthwestEntries end");
+  console.debug("jme modifySouthwestEntries end");
 }
 
 
 function modifySouthwestEntry(flightListItem)
 {
-  console.log("jme modifySouthwestEntry begin");
-  console.log(flightListItem);
+  console.debug("jme modifySouthwestEntry begin");
+  console.debug(flightListItem);
 
   var priceXpathResult = document.evaluate(
     ".//jsl[text()='Price unavailable']",
@@ -58,15 +99,11 @@ function modifySouthwestEntry(flightListItem)
   for(var priceIdx = 0; priceIdx < priceXpathResult.snapshotLength; priceIdx++)
   {
     var priceNode = priceXpathResult.snapshotItem(priceIdx);
-    priceNode.innerText = "PRICE_GOES_HERE";
+    priceNode.innerText = "PRICE_GOES_HERE_" + priceIdx;
   }
 
-  console.log("jme modifySouthwestEntry end");
+  console.debug("jme modifySouthwestEntry end");
 }
 
 
-(new MutationObserver(checkForResults)).observe(
-  document,
-  {childList: true, subtree: true});
-
-console.log("jme end");
+console.debug("jme end");
